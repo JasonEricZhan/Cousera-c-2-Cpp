53f0258cbcab4155cf19cb16bb8362d0868bc864
JasonEricZhan <jan11781202@gmail.com>
2017-07-25 02:38:42 +0800

    Update shortestpath.cpp

--- hw1/shortestpath.cpp
+++ hw1/shortestpath.cpp
@@ -29,14 +29,14 @@ void ShortestPath::back_track(int start,int parent,vector<vertex> &vertexMatrix)
 
 
 
-void relax(int u,int v,vector<vector<double> > &costMatrix,vector<vertex> &vertexMatrix)
+void relax(int u,int v,graph &g)
 {
-    if(vertexMatrix[v].distance > vertexMatrix[u].distance
-       +costMatrix[u][v])
+    if(g.vertexqueue[v].distance > g.vertexqueue[u].distance
+       +g.getedge_value(u,v))
     {
-        vertexMatrix[v].distance=vertexMatrix[u].distance
-        +costMatrix[u][v];
-        vertexMatrix[v].parent=u;
+        g.vertexqueue[v].distance=g.vertexqueue[u].distance
+        +g.getedge_value(u,v);
+        g.vertexqueue[v].parent=u;
     }
 }
 
@@ -45,38 +45,38 @@ void relax(int u,int v,vector<vector<double> > &costMatrix,vector<vertex> &verte
 void ShortestPath::path(graph &g,int v1,int v2)
 {
     //cout<<"start"<<endl;
-    //Dijkstra's algo:
+    //Dijakstra:
     PriorityQueue *p=new PriorityQueue;
     for(int i = v1+1;i<v2+1;i++)
     {
         QueueNode v;
         v.vertex=i;
-          g.vertexMatrix[i].distance=g.costMatrix[v1][i];
+          g.vertexqueue[i].distance=g.getedge_value(v1,i);
         
-           v.priority=g.vertexMatrix[i].distance;
-           if(g.adMatrix[v1][i]==1)
+           v.priority=g.vertexqueue[i].distance;
+           if(g.isadjacent(v1,i))
            {
-             g.vertexMatrix[i].parent=v1;
+             g.vertexqueue[i].parent=v1;
            }
         
         p->insert(v);
     }
-    g.vertexMatrix[v1].distance=0;
-    g.vertexMatrix[v1].parent=v1;
+    g.vertexqueue[v1].distance=0;
+    g.vertexqueue[v1].parent=v1;
     while(p->size()>1)
     {
         QueueNode min=p->remove_min();
         for(int i=v1;i<v2+1;i++)
         {
-                if(g.adMatrix[min.vertex][i]==1 && i!=min.vertex)
+                if(g.isadjacent(min.vertex,i) && i!=min.vertex)
                 {
-                    relax(min.vertex,i,g.costMatrix,g.vertexMatrix);
+                    relax(min.vertex,i,g);
                 }
         }
         
     }
     //start record
-    if(g.vertexMatrix[v2].parent==-1)
+    if(g.vertexqueue[v2].parent==-1)
     {
         cout<<"no such path"<<endl;
     }
@@ -85,7 +85,7 @@ void ShortestPath::path(graph &g,int v1,int v2)
         //cout<<"path is"<<endl;
         //cout<<v2<<endl;
         pathQueue.push_back(v2);
-        back_track(v1,g.vertexMatrix[v2].parent,g.vertexMatrix);
+        back_track(v1,g.vertexqueue[v2].parent,g.vertexqueue);
     }
 }
 
@@ -93,7 +93,7 @@ void ShortestPath::path(graph &g,int v1,int v2)
 int ShortestPath::path_size(graph &g,int v1,int v2)
 {
     path(g,v1,v2);
-    if(g.vertexMatrix[v2].parent!=-1)
+    if(g.vertexqueue[v2].parent!=-1)
     {
         if(pathQueue[0]!=v1 || pathQueue.back()!=v2)
         {
