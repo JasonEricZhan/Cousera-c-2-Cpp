07f1a19c55f68898411638ca283e6726aec7187b
JasonEricZhan <jan11781202@gmail.com>
2017-07-25 01:59:35 +0800

    Update shortestpath.cpp

--- hw1/shortestpath.cpp
+++ hw1/shortestpath.cpp
@@ -46,7 +46,6 @@ void ShortestPath::path(graph &g,int v1,int v2)
 {
     //cout<<"start"<<endl;
     //Dijkstra's algo:
-    int n=static_cast<int>(g.costMatrix.size());
     PriorityQueue *p=new PriorityQueue;
     for(int i = v1+1;i<v2+1;i++)
     {
